a87de8a4d1c2e9bf191bd4e31caeb659dc16c36e
Son Thanh <coderthanhson@gmail.com>
2023-01-31 12:47:29 +0700

    update file

--- Search Tags/app.js
+++ Search Tags/app.js
@@ -1,78 +1,38 @@
-var images = document.querySelectorAll('.image img');
-var prev = document.querySelector('.prev');
-var next = document.querySelector('.next');
-var close = document.querySelector('.close');
-var gallleryImg = document.querySelector('.gallery__inner img');
-var galllery = document.querySelector('.gallery');
+var content = document.querySelector('.content');
+var input = document.querySelector('.content input');
+var btnRemoveAll = document.querySelector('.remove-all');
 
-var currentIndex = 0;
-function showGallery() {
-    if (currentIndex == 0) {
-        prev.classList.add('hide');
-    } else {
-        prev.classList.remove('hide');
-    }
+var tags = ['Nodejs', 'Reactjs']
 
-    if (currentIndex == images.length - 1) {
-        next.classList.add('hide');
-    } else {
-        next.classList.remove('hide');
+// function
+function render() {
+    content.innerHTML = '';
+    for (let i = 0; i < tags.length; i++) {
+        const tag = tags[i];
+        content.innerHTML += `<li>${tag}<i class="fas fa-times" onclick="removeTag(${i})"></i></li>`
     }
-    //display
-    gallleryImg.src = images[currentIndex].src;
-    galllery.classList.add('show');
+    content.appendChild(input);
+    input.focus();
 }
-// function loopGallery() {
-//     if (currentIndex < images.length - 1) {
-//         currentIndex++;
-//         showGallery();
-//     } else {
-//         currentIndex = 0;
-//     }
-// }
-images.forEach((item, index) => {
-    item.addEventListener('click', function () {
-        currentIndex = index;
-        showGallery();
-        onkeydown = function (event) {
-            var e = event || window.event;
-            if (e.keyCode == '37' && currentIndex > 0) {
-                //button left
-                currentIndex--;
-                showGallery();
-            } else if (e.keyCode == '39' && currentIndex < images.length - 1) {
-                //buttin right
-                currentIndex++;
-                showGallery();
-            }
+function removeTag(index) {
+    tags.splice(index, 1);
+    render();
+}
+// listener
+render();
+input.addEventListener('keydown', function (event) {
+    if (!tags.includes(input.value.trim()) && input.value.trim() != "") {
+        if (event.key == 'Enter') {
+            tags.push(input.value.trim());
+            input.value = '';
+            render();
+            console.log(tags);
         }
-        // setInterval(loopGallery, 1000);
-
-    })
-})
-
-close.addEventListener('click', function () {
-    galllery.classList.remove('show');
-})
-
-document.addEventListener('keydown', function (e) {
-    if (e.keyCode == 27) {
-        galllery.classList.remove('show');
+    } else {
+        input.value = '';
     }
 })
-
-
-
-prev.addEventListener('click', function () {
-    if (currentIndex > 0) {
-        currentIndex--;
-        showGallery();
-    }
-})
-
-next.addEventListener('click', function () {
-    if (currentIndex < images.length - 1) {
-        currentIndex++;
-        showGallery();
-    }
+btnRemoveAll.addEventListener('click', function () {
+    tags = [];
+    render();
 })
